965be22d49dc2b1b7053c21b6ad60d5e1e490ced
Chelsea486MHz <mail@chelsea486mhz.fr>
2023-07-15 01:02:29 +0200

    Added environment variable support

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 VAULT_SECRET_PATH = 'secret/gpg-keys'
-VAULT_HOST = 'localhost'
-VAULT_PORT = '8200'
+VAULT_HOST = os.environ.get('VAULT_HOST', 'localhost')
+VAULT_PORT = os.environ.get('VAULT_PORT', '8200')
 
 VAULT_URI = 'http://' + VAULT_HOST + ':' + VAULT_PORT
 
-DATABASE_NAME = 'manta'
-DATABASE_HOST = 'localhost'
-DATABASE_USER = 'manta-user'
-DATABASE_PASS = 'manta-user-pw'
+DATABASE_NAME = os.environ.get('DATABASE_NAME', 'manta')
+DATABASE_HOST = os.environ.get('DATABASE_HOST', 'locahost')
+DATABASE_USER = os.environ.get('DATABASE_USER', 'manta-user')
+DATABASE_PASS = os.environ.get('DATABASE_PASS', 'manta-user-pw')
 
 DATABASE_URI = 'mysql+pymysql://' + DATABASE_USER + ':' + DATABASE_PASS + '@' + DATABASE_HOST + '/' + DATABASE_NAME
